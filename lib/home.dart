6d616b01ec4305f428e4223febf48ff1d7acd814
Yoshihiro Tanaka <contact@cordea.jp>
2021-07-23 16:49:26 +0900

    draw light shade

--- lib/home.dart
+++ lib/home.dart
@@ -18,9 +18,14 @@ class HomePage extends StatelessWidget {
 }
 
 class _IluminarPainter extends CustomPainter {
-  static const _startWidth = 0.2;
-  static const _endWidth = 0.7;
+  static const double _cableWidth = 4;
+  static const double _cableHeight = 5;
+  static const double _lightShadeWidth = 90;
+  static const double _lightShadeHeight = 50;
+  static const double _startWidth = 0.2;
+  static const double _endWidth = 0.7;
 
+  final _lightPaint = Paint()..color = Colors.black;
   final _foregroundPaint = Paint()..color = Colors.black;
   final _backgroundPaint = Paint()
     ..color = Colors.yellow.withAlpha((255 * 0.4).toInt());
@@ -31,6 +36,33 @@ class _IluminarPainter extends CustomPainter {
     canvas.drawRect(rect, _backgroundPaint);
 
     final centerX = size.width / 2;
+    final lightShadePath = Path()
+      ..moveTo(centerX - _cableWidth / 2, 0)
+      ..relativeLineTo(_cableWidth, 0)
+      ..relativeLineTo(0, _cableHeight)
+      ..relativeLineTo(-_cableWidth, 0)
+      ..close()
+      ..moveTo(centerX - _cableWidth / 2, _cableHeight)
+      ..relativeCubicTo(
+        -_lightShadeWidth / 4,
+        0,
+        -_lightShadeWidth / 2 + _lightShadeWidth / 20,
+        _lightShadeHeight / 3,
+        -_lightShadeWidth / 2,
+        _lightShadeHeight,
+      )
+      ..relativeLineTo(_lightShadeWidth + _cableWidth, 0)
+      ..relativeCubicTo(
+        -_lightShadeWidth / 20,
+        -_lightShadeHeight * 2 / 3,
+        -_lightShadeWidth / 4,
+        -_lightShadeHeight,
+        -_lightShadeWidth / 2 + _cableWidth / 2,
+        -_lightShadeHeight,
+      )
+      ..close();
+    canvas.drawPath(lightShadePath, _lightPaint);
+
     final startWidth = size.width * _startWidth;
     final endWidth = size.width * _endWidth;
     final lightPath = Path()
