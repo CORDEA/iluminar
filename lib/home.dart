e6f9414a1946881c54194616d5eee35369fbf3d1
Yoshihiro Tanaka <contact@cordea.jp>
2021-07-23 15:14:35 +0900

    add painter

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: CustomPaint(
+        foregroundPainter: _IluminarPainter(),
+        child: Center(),
+      ),
     );
+  }
+}
+
+class _IluminarPainter extends CustomPainter {
+  final _paint = Paint()..color = Colors.black;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), _paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
