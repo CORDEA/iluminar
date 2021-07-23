9503739abab275e3a11c47a17fca748d85875119
Yoshihiro Tanaka <contact@cordea.jp>
2021-07-23 16:07:08 +0900

    draw light

--- lib/home.dart
+++ lib/home.dart
@@ -6,18 +6,45 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: CustomPaint(
         foregroundPainter: _IluminarPainter(),
-        child: Center(),
+        child: Center(
+          child: Text(
+            'Iluminar',
+            style: Theme.of(context).textTheme.headline2,
+          ),
+        ),
       ),
     );
   }
 }
 
 class _IluminarPainter extends CustomPainter {
-  final _paint = Paint()..color = Colors.black;
+  static const _startWidth = 0.2;
+  static const _endWidth = 0.7;
+
+  final _foregroundPaint = Paint()..color = Colors.black;
+  final _backgroundPaint = Paint()
+    ..color = Colors.yellow.withAlpha((255 * 0.4).toInt());
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), _paint);
+    final rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    canvas.drawRect(rect, _backgroundPaint);
+
+    final centerX = size.width / 2;
+    final startWidth = size.width * _startWidth;
+    final endWidth = size.width * _endWidth;
+    final lightPath = Path()
+      ..moveTo(centerX - startWidth / 2, 0)
+      ..lineTo(centerX + startWidth / 2, 0)
+      ..lineTo(centerX + endWidth / 2, size.height)
+      ..lineTo(centerX - endWidth / 2, size.height)
+      ..close();
+    final basePath = Path()
+      ..addRect(Rect.fromLTWH(0, 0, size.width, size.height));
+    final path = Path.combine(PathOperation.difference, basePath, lightPath);
+
+    canvas.clipPath(path);
+    canvas.drawPath(path, _foregroundPaint);
   }
 
   @override
