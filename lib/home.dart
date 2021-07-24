319bbe553482cb3e455300ab3f5665ff15d111cf
Yoshihiro Tanaka <contact@cordea.jp>
2021-07-24 14:03:34 +0900

    update

--- lib/home.dart
+++ lib/home.dart
@@ -7,13 +7,72 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: AnimatedIluminarContainer(
-        builder: (context) => Center(
+        builder: (context) => const _HomeContent(),
+      ),
+    );
+  }
+}
+
+class _HomeContent extends StatelessWidget {
+  const _HomeContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      children: [
+        const SizedBox(height: 64),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Text(
-            'Iluminar',
-            style: Theme.of(context).textTheme.headline2,
+            'cordea',
+            style: Theme.of(context).textTheme.headline2!.copyWith(
+                  color: Colors.black,
+                ),
           ),
         ),
-      ),
+        const SizedBox(height: 24),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            'Yoshihiro Tanaka',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            'Android/Flutter applications engineer',
+            style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                  color: Colors.black54,
+                ),
+          ),
+        ),
+        const SizedBox(height: 24),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            'SNS',
+            style: Theme.of(context).textTheme.headline5!.copyWith(
+                  color: Colors.black,
+                ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        ListTile(
+          title: Text('GitHub'),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text('LinkedIn'),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text('Medium'),
+          onTap: () {},
+        ),
+      ],
     );
   }
 }
